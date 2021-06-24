7420e811a525278b4ce4dd68416708603057b529
Sharvari Thakar <Sharvari.Thakar@green-apex.com>
2021-06-24 13:39:55 +0530

    trash changes

--- src/app/components/trash/trash.component.ts
+++ src/app/components/trash/trash.component.ts
@@ -47,7 +47,6 @@ export class TrashComponent implements OnInit {
       delete: () => {
         let index = this.trash.findIndex(x => x.id === id);
         this.trash.splice(index, 1);
-        ///Demo
         this.productService.setProducts()
         dialog.close()
       }
